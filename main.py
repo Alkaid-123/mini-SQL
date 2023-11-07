0bd21f3584adbd08f81b39d129c50ad2ddfa92d6
a <916508192@qq.com>
2023-11-08 00:25:55 +0800

    select

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 import datetime
 import json
 import csv
-from prettytable import PrettyTable
-
+from prettytable import PrettyTable, from_csv
 
 database='test2'
 
@@ -12,6 +11,7 @@ def get_command():
     command = ''
     while not command.strip():
         command = input("sql> ").lower().strip() + ';'
+    command=re.sub(r'\s+',' ',command)
     print(command)
     return command
 
@@ -27,7 +27,7 @@ def check_command(command):
         re.compile(r'delete from \w+(\s+where (\w+\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+)\s*and\s*)*\s*\w+\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+)\s*)?;'),
         re.compile(r'drop database [\w]+[\s]*;'),
         re.compile(r'drop table [\w]+[\s]*;'),
-        re.compile(r'select ([\w]+(,[\w]+)*|\*) from [\w]+(\s+where (\w+\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+)\s*and\s*)*\s*\w+\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+)\s*)?;'),
+        re.compile(r'select (\w+(,\s*\w+)*|\*) from (\w+,\s*)*\w+(\s+where ([\w\.]+\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+|[\w\.]+)\s*and\s*)*\s*[\w\.]+\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+|[\w\.]+)\s*)?;'),
     ]
 
     for pattern in patterns:
@@ -82,6 +82,14 @@ def drop_database(command):
         for item in database_list:
             writer.writerow(item)
 
+
+def print_table(data):
+    table = PrettyTable()
+    table.field_names = data[0]
+    for row in data[1:]:
+        table.add_row(row)
+    print(table)
+
 def desc(command):
     global database
     if(database==''):
@@ -99,21 +107,7 @@ def desc(command):
     # TODO not_null not null
     for item in table_dict[table_name]:
         data.append([item['name'], item['type'], item['length'], item['not null'], item['primary_key']])
-    # column_widths = [max(len(str(item)) for item in column) for column in zip(*data)]
-    # separator = "+".join("-" * (width + 2) for width in column_widths)
-    # print("+" + separator + "+")
-    # print("| " + " | ".join(str(item).ljust(width) for item, width in zip(data[0], column_widths)) + " |")
-    # print("+" + separator + "+")
-    # data=data[1:]
-    # for row in data:
-    #     print("| " + " | ".join(str(item).ljust(width) for item, width in zip(row, column_widths)) + " |")
-    # print("+" + separator + "+")
-    table = PrettyTable()
-    table.field_names = data[0]
-    for row in data[1:]:
-        table.add_row(row)
-    print(table)
-
+    print_table(data)
     return
 
 def create_table(command):
@@ -225,55 +219,174 @@ def delete_from(command):
             writer.writerow(table_data[0])
     return
 
+
+def cartesian_product(tables):
+    if len(tables) == 1:
+        return tables[0]
+    result = []
+    rest_product = cartesian_product(tables[1:])
+    for item in tables[0]:
+        for product_item in rest_product:
+            result.append(item + product_item)
+    
+    return result
+
+
+def find_index(tables,name,table_name_list,header):
+    result=re.search(r'([\w]+)\.([\w]+)',name)
+    if result:
+        table_name=result.group(1)
+        column_name=result.group(2)
+        table_index=table_name_list.index(table_name)
+        column_index=header[table_index].index(column_name)
+    else:
+        for i,table in enumerate(tables):
+            if name in table[0]:
+                table_index=i
+                column_index=table[0].index(name)
+                break
+    return [table_index,column_index]
+
+
+
+
 def select(command):
     global database
     if(database==''):
         print("No database in use!")
         return
-    table_name = re.search(r'select ([\w]+|\*) from ([\w]+)',command).group(2)
-    if os.path.exists(database+'/'+table_name+'.csv')==False:
-        print("Table not exists!")
-        return
-    with open(database+'/'+table_name+'.csv','r') as f:
-        reader = csv.reader(f)
-        table_data = [row for row in reader]
+    table_name = re.search(r'from ([\w,\s]+)(where|;)',command).group(1)
+    table_name=table_name.replace(' ','')
+    table_name_list = table_name.split(',')
+    print(table_name_list)
+
+    with open(database+'/system_table.json','r') as f:
+        table_dict=json.load(f)
+
+    tables=[]
+    header=[]
+    # datas=[]
+    for table_name in table_name_list:
+        if os.path.exists(database+'/'+table_name+'.csv')==False:
+            print("Table '"+ table_name +"' not exists!")
+            return
+        with open(database+'/'+table_name+'.csv','r') as f:
+            reader = csv.reader(f)
+            table_data = [row for row in reader]
+
+        # 将type为int的列转换为int
+        for item in table_dict[table_name]:
+            if item['type'] == 'int':
+                table_data = [[int(item) if item.isdigit() else item for item in row] for row in table_data]
+
+        header.append(table_data[0])
+        # datas.append(table_data[1:])
+        tables.append(table_data)
+    
+    print(tables)
+    
+
+
+    # join and select操作
     is_where = re.search(r'where (.*);',command)
+    select_list = []
+    join_list = []
     if is_where:
-        return
         where=is_where.group(1)
         # 将操作数和操作符提取出来
-        where_list=re.findall(r'(\w+)\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+)',where)
-        # 将字符串中多余引号去掉
-        print(where_list)
-    else:
-        # TODO 封装函数
-        data = table_data
-        # 考虑中文情况
+        where_list=re.findall(r'([\w\.]+)\s*(=|>|<|>=|<=|!=)\s*(\'[\w\u4e00-\u9fa5\-]+\'|\d+|[\w\.]+)',where)
+
+        where_list=[list(item) for item in where_list]
+
         
+        for item in where_list:
+            if item[2].startswith('\''):
+                item[2] = item[2].strip('\'')
+                select_list.append(item)
+            elif item[2].isdigit():
+                item[2] = int(item[2])
+                select_list.append(item)
+            else:
+                join_list.append(item)
+        print(where_list)
+        print(select_list)
+        print(join_list)
+        
+        # select操作
+        # TODO 得去自己里面找 不能用笛卡尔积 不然所有人都会满足条件 也就是更新tables
+        for item in select_list:
+            item[0]=find_index(tables,item[0],table_name_list,header)
+        print(select_list)
+        for item in join_list:
+            item[0]=find_index(tables,item[0],table_name_list,header)
+            item[2]=find_index(tables,item[2],table_name_list,header)
+        print(join_list)
 
-        # column_widths = [max(len(str(item)) for item in column) for column in zip(*data)]
+        for item in select_list:
+            table=tables[item[0][0]]
+            if item[1]=='=':
+                table=[table[0]]+[row for row in table[1:] if row[item[0][1]]==item[2]]
+            elif item[1]=='>':
+                table=[table[0]]+[row for row in table[1:] if row[item[0][1]]>item[2]]
+            elif item[1]=='<':
+                table=[table[0]]+[row for row in table[1:] if row[item[0][1]]<item[2]]
+            elif item[1]=='>=':
+                table=[table[0]]+[row for row in table[1:] if row[item[0][1]]>=item[2]]
+            elif item[1]=='<=':
+                table=[table[0]]+[row for row in table[1:] if row[item[0][1]]<=item[2]]
+            elif item[1]=='!=':
+                table=[table[0]]+[row for row in table[1:] if row[item[0][1]]!=item[2]]
+            tables[item[0][0]]=table
+        print('selected tables',tables)
 
-        # column_widths = [0] * len(data[0])
-        # for row in data:
-        #     for i, cell in enumerate(row):
-        #         cell_length = sum(2 if ord(c) > 127 else 1 for c in cell)
-        #         column_widths[i] = max(column_widths[i], cell_length)
+        # join操作
+        # TODO 这只是两表合并 还有多表合并
+        if len(join_list)==len(table_name_list)-1 and len(join_list)!=0:
+            merge_table=[]
+            for item in join_list:
+                table1=tables[item[0][0]]
+                table2=tables[item[2][0]]
+                # 如果table1中某一行当中item[0][1]列的值和table2中某一行item[2][1]列的值相等 则将这两行合并
+                merge_table.append(table1[0]+table2[0])
+                for row1 in table1[1:]:
+                    for row2 in table2[1:]:
+                        if row1[item[0][1]]==row2[item[2][1]]:
+                            merge_table.append(row1+row2)
+                tables[item[0][0]]=merge_table
+                tables[item[2][0]]=merge_table 
+                print('tables\n',tables)
+            tables=merge_table
 
-        # print(column_widths)
-        # separator = "+".join("-" * (width + 2) for width in column_widths)
-        # print("+" + separator + "+")
-        # print("|" + "|".join(str(item).ljust(width,chr(12288)) for item, width in zip(data[0], column_widths)) + "|")
-        # print("+" + separator + "+")
-        # data=data[1:]
-        # for row in data:
-        #     print("|" + "|".join(str(item).ljust(width,chr(12288)) for item, width in zip(row, column_widths)) + "|")
-        # print("+" + separator + "+")
+            
 
-        table = PrettyTable()
-        table.field_names = data[0]
-        for row in data[1:]:
-            table.add_row(row)
-        print(table)
+    # TODO 封装函数
+    # 笛卡尔积
+    if len(join_list)==len(table_name_list)-1 and len(join_list)!=0:
+        pass
+    elif len(join_list)!=len(table_name_list)-1:
+        datas=[]
+        for table in tables:
+            datas.append(table[1:])
+        header=[item for sublist in header for item in sublist]
+        tables=[header]+cartesian_product(datas)
+    else:
+        tables=tables[0]
+
+    # project操作
+    project=re.search(r'select (.*) from',command).group(1)
+    project_list=project.split(',')
+    print(project_list)
+    if project_list[0]=='*':
+        pass
+    else:
+        project_list=[item.strip() for item in project_list]
+        project_list=[tables[0].index(item) for item in project_list]
+        header=[tables[0][i] for i in project_list]
+        tables=[header]+[[item[i] for i in project_list] for item in tables[1:]]
+    # print(tables)
+    # TODO tables[0???
+    print_table(tables)
+    print('共有'+str(len(tables)-1)+'条记录')
 
 if __name__ == '__main__':
     command_functions = {
@@ -298,3 +411,6 @@ if __name__ == '__main__':
             if re.match(cmd, command):
                 func(command)
                 break
+
+
+# TODO 各种异常处理 不要退出程序
